93655760ce9cbebef8d05e6e314f8231187ac204
deepak <deepaksebak4@gmail.com>
2022-11-12 05:58:54 +0530

    Create the header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Offer from "./pages/Offer";
 import ForgotPassword from "./pages/ForgotPassword";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  function checkPath(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="max-w-6xl mx-auto flex justify-between items-center px-4">
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className="h-5 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                checkPath("/") && " border-b-red-500 text-black"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                checkPath("/offers") && " border-b-red-500 text-black"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                checkPath("/sign-in") && " border-b-red-500 text-black"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
